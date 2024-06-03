c809e076e03938b356eaee1c39e879ed108fe8d4
samuel-videau <samuel.videau@efrei.net>
2024-06-03 21:42:25 +1000

    feat: loading and modal on account creation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,12 +4,15 @@ import { useLocalStorage, STORAGE_KEY } from '@/libs/useLocalStorage';
 import { useApi } from "@/libs/useApi";
 import { setUser } from '@/store/user-reducer';
 import { useAppDispatch } from '@/store/hooks';
+import Modal from "@/components/modal";
 
 export default function Navbar() {
   const [userId, setUserId] = useState<string | null>(null);
   const { createUser } = useApi();
   const { getValue, setValue } = useLocalStorage();
   const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState<boolean>(false);
+  const [modal, setModal] = useState<{ type: 'success' | 'error', title: string, message: string, subMessage?: string } | null>(null);
 
   const isConnected = () => !!userId;
 
@@ -26,6 +29,7 @@ export default function Navbar() {
   }, []);
 
   const handleConnect = async () => {
+    setLoading(true);
     if (!isConnected()) {
       try {
         const user = await createUser();
@@ -33,24 +37,33 @@ export default function Navbar() {
         setValue(STORAGE_KEY.USER_ID, user.id);
       } catch (error) {
         console.error("Failed to create account:", error);
+        setModal({ type: 'error', title: 'Failed to create account', message: 'There was an error creating your account. Please try again.' });
       }
     } else {
       setUserId(null);
       setValue(STORAGE_KEY.USER_ID, '');
+      dispatch(setUser({id: ''}));
     }
+    setLoading(false);
+  };
+
+  const closeModal = () => {
+    setModal(null);
   };
 
   return (
     <nav className="bg-blue-600 py-4 text-white flex justify-between px-4">
+      {modal && <Modal type={modal.type} title={modal.title} message={modal.message} subMessage={modal.subMessage} onClose={closeModal} />}
       <div className="flex space-x-4">
         <Link href="/campaigns" className="hover:underline">Campaigns</Link>
         <Link href="/keys" className="hover:underline">Keys</Link>
       </div>
       <button
         onClick={handleConnect}
-        className="ml-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+        className="ml-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 disabled:opacity-50"
+        disabled={loading}
       >
-        {isConnected() ? 'Disconnect' : 'Create Account'}
+        {loading ? "Processing..." : isConnected() ? 'Disconnect' : 'Create Account'}
       </button>
     </nav>
   );
